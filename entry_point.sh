945a05da276d87484c15b6ff7b443011c3e536b0
TNE <tne@garudalinux.org>
2024-01-23 16:40:56 +0100

    fix(manager): Preserve ssh tunnel instead

--- entry_point.sh
+++ entry_point.sh
@@ -4,27 +4,23 @@ set -e
 
 export REDIS_PORT="${REDIS_PORT:-6379}"
 
-if [ -f /tmp/tunnel.pid ]; then
-    echo "Killing old tunnel"
-    kill $(cat /tmp/tunnel.pid)
-    rm /tmp/tunnel.pid
-fi
-
 if [ -n "$REDIS_SSH_HOST" ]; then
     REDIS_SSH_PORT="${REDIS_SSH_PORT:-22}"
     REDIS_SSH_USER="${REDIS_SSH_USER:-root}"
 
-    # Set up ssh tunneling
-    AUTOSSH_PIDFILE=/tmp/tunnel.pid AUTOSSH_GATETIME=0 AUTOSSH_PORT=0 autossh -f -N -L "6380:127.0.0.1:${REDIS_PORT}" \
-        -p "$REDIS_SSH_PORT" \
-        -o StrictHostKeyChecking=no \
-        -o ServerAliveInterval=60 \
-        -o ServerAliveCountMax=3 \
-        -o ExitOnForwardFailure=yes \
-        -o ConnectTimeout=10 \
-        -o TCPKeepAlive=yes \
-        -i /app/sshkey \
-        "$REDIS_SSH_USER@$REDIS_SSH_HOST"
+    if [ ! -f /tmp/tunnel.pid ]; then
+        # Set up ssh tunneling
+        AUTOSSH_PIDFILE=/tmp/tunnel.pid AUTOSSH_GATETIME=0 AUTOSSH_PORT=0 autossh -f -N -L "6380:127.0.0.1:${REDIS_PORT}" \
+            -p "$REDIS_SSH_PORT" \
+            -o StrictHostKeyChecking=no \
+            -o ServerAliveInterval=60 \
+            -o ServerAliveCountMax=3 \
+            -o ExitOnForwardFailure=yes \
+            -o ConnectTimeout=10 \
+            -o TCPKeepAlive=yes \
+            -i /app/sshkey \
+            "$REDIS_SSH_USER@$REDIS_SSH_HOST"
+    fi
 
     export REDIS_PORT=6380
 
