19785c003bdfcc088d28b08b7c2fc531978b54ac
TNE <tne@garudalinux.org>
2024-01-23 15:56:55 +0100

    fix(manager): Kill old autossh tunnel automatically

--- entry_point.sh
+++ entry_point.sh
@@ -4,12 +4,18 @@ set -e
 
 export REDIS_PORT="${REDIS_PORT:-6379}"
 
+if [ -f /tmp/tunnel.pid ]; then
+    echo "Killing old tunnel"
+    kill $(cat /tmp/tunnel.pid)
+    rm /tmp/tunnel.pid
+fi
+
 if [ -n "$REDIS_SSH_HOST" ]; then
     REDIS_SSH_PORT="${REDIS_SSH_PORT:-22}"
     REDIS_SSH_USER="${REDIS_SSH_USER:-root}"
 
     # Set up ssh tunneling
-    AUTOSSH_GATETIME=0 AUTOSSH_PORT=0 autossh -f -N -L "6380:127.0.0.1:${REDIS_PORT}" \
+    AUTOSSH_PIDFILE=/tmp/tunnel.pid AUTOSSH_GATETIME=0 AUTOSSH_PORT=0 autossh -f -N -L "6380:127.0.0.1:${REDIS_PORT}" \
         -p "$REDIS_SSH_PORT" \
         -o StrictHostKeyChecking=no \
         -o ServerAliveInterval=60 \
