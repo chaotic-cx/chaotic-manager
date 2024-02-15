5d704e49b17d3c23a1ce22f7cd01e9daeafef32b
TNE <tne@garudalinux.org>
2024-02-15 08:22:20 +0100

    fix(manager/builder): Fix edgecase where dependents would not be notified if build fails

--- src/builder.ts
+++ src/builder.ts
@@ -17,7 +17,7 @@ function ensurePathClean(dir: string): void {
 }
 
 function requestRemoteConfig(manager: RedisConnectionManager, worker: Worker, docker: DockerManager, config: any): Promise<void> {
-    return new Promise<void>(async (resolve, reject) => { 
+    return new Promise<void>(async (resolve, reject) => {
         const database_host = process.env.DATABASE_HOST || null;
         const database_port = Number(process.env.DATABASE_PORT || 22);
         const database_user = process.env.DATABASE_USER || null;
@@ -31,13 +31,12 @@ function requestRemoteConfig(manager: RedisConnectionManager, worker: Worker, do
                     console.log("Pausing worker for incoming config...");
                     worker.pause();
                 }
-                const remote_config : RemoteSettings = JSON.parse(message);
+                const remote_config: RemoteSettings = JSON.parse(message);
                 if (remote_config.version !== current_version) {
                     console.log("Worker received incompatible config from master. Worker paused.");
                     return;
                 }
-                else
-                {
+                else {
                     if (database_host !== null) {
                         remote_config.database.ssh.host = database_host;
                         remote_config.database.ssh.port = database_port;
@@ -50,8 +49,7 @@ function requestRemoteConfig(manager: RedisConnectionManager, worker: Worker, do
                         init = false;
                         resolve();
                     }
-                    else
-                    {
+                    else {
                         worker.resume();
                         console.log("Worker received valid config from master. Worker resumed.");
                     }
@@ -137,12 +135,12 @@ export default function createBuilder(redis_connection_manager: RedisConnectionM
     const database_queue = new Queue("database", { connection });
     const builds_queue = new Queue("builds", { connection });
 
-    const runtime_settings : { settings: RemoteSettings | null } = { settings: null };
+    const runtime_settings: { settings: RemoteSettings | null } = { settings: null };
 
     const worker = new Worker("builds", async (job: Job) => {
         if (job.id === undefined)
             throw new Error('Job ID is undefined');
-        
+
         const { target_repo, pkgbase } = splitJobId(job.id);
         const logger = new BuildsRedisLogger(connection);
         logger.fromJob(job);
@@ -156,8 +154,7 @@ export default function createBuilder(redis_connection_manager: RedisConnectionM
             const pending_deps = await anyDependencyPending(jobdata, job.id, builds_queue);
             if (pending_deps && pending_deps.pending > 0) {
                 var logstring = `Job ${job.id} has ${pending_deps.pending} pending dependencies, of which ${pending_deps.pending_know} know about this job.`;
-                if (pending_deps.pending_know > 0)
-                {
+                if (pending_deps.pending_know > 0) {
                     logstring += " Delaying job until dependencies are resolved.";
                     logger.log(logstring);
 
@@ -165,72 +162,74 @@ export default function createBuilder(redis_connection_manager: RedisConnectionM
                     job.moveToDelayed(Date.now() + 24 * 60 * 60 * 1000, job.token);
                     throw new DelayedError("Job delayed due to pending dependencies.");
                 }
-                else
-                {
+                else {
                     logstring += " Proceeding with build.";
                     logger.log(logstring);
                 }
             }
         }
-
-        const repo_manager: RepoManager = new RepoManager(undefined);
-        repo_manager.repoFromObject(remote_settings.repos);
-        repo_manager.targetRepoFromObject(remote_settings.target_repos);
-        const src_repo = repo_manager.getRepo(jobdata.srcrepo);
-
-        ensurePathClean(mount);
-        const [err, out] = await docker_manager.run(remote_settings.builder.image, ["build", pkgbase], [shared_pkgout + ':/home/builder/pkgout', shared_sources + ':/pkgbuilds'], [
-            "PACKAGE_REPO_ID=" + src_repo.id,
-            "PACKAGE_REPO_URL=" + src_repo.getUrl(),
-            "EXTRA_PACMAN_REPOS=" + repo_manager.getTargetRepo(target_repo).repoToString(),
-            "EXTRA_PACMAN_KEYRINGS=" + repo_manager.getTargetRepo(target_repo).keyringsToBashArray(),
-        ], logger.raw_log.bind(logger));
-
-        const file_list = fs.readdirSync(mount);
-        if (err || out.StatusCode !== undefined || file_list.length === 0) {
-            logger.log(`Job ${job.id} failed`);
-            throw new Error('Building failed.');
-        }
-        else
-            logger.log(`Finished build ${job.id}. Uploading...`);
-
-        const sshlogger = new SshLogger();
         try {
-            const client = await Client({
-                host: String(remote_settings.database.ssh.host),
-                port: Number(remote_settings.database.ssh.port),
-                username: String(remote_settings.database.ssh.user),
-                privateKey: fs.readFileSync('sshkey'),
-                debug: sshlogger.log.bind(sshlogger)
-            })
-            await client.uploadDir(
-                mount,
-                remote_settings.database.landing_zone,
-            )
-            client.close()
-        } catch (e) {
-            logger.error(`Failed to upload ${job.id}: ${e}`);
-            // This does not get logged to redis
-            console.error(sshlogger.dump());
-            console.log("End of ssh log.");
-            throw new Error('Upload failed.');
-        }
-        logger.log(`Finished upload ${job.id}.`);
-        const db_job_data: DatabaseJobData = {
-            arch: jobdata.arch,
-            packages: file_list,
-            timestamp: jobdata.timestamp,
-            commit: jobdata.commit,
-            srcrepo: jobdata.srcrepo,
-        };
-        await database_queue.add("database", db_job_data, {
-            jobId: job.id,
-            removeOnComplete: true,
-            removeOnFail: true,
-        });
-        logger.log(`Build job ${job.id} finished. Scheduled database job at ${new Date().toISOString()}...`);
+            const repo_manager: RepoManager = new RepoManager(undefined);
+            repo_manager.repoFromObject(remote_settings.repos);
+            repo_manager.targetRepoFromObject(remote_settings.target_repos);
+            const src_repo = repo_manager.getRepo(jobdata.srcrepo);
 
-        setTimeout(promotePendingDependents.bind(null, jobdata, builds_queue, logger), 1000);
+            ensurePathClean(mount);
+            const [err, out] = await docker_manager.run(remote_settings.builder.image, ["build", pkgbase], [shared_pkgout + ':/home/builder/pkgout', shared_sources + ':/pkgbuilds'], [
+                "PACKAGE_REPO_ID=" + src_repo.id,
+                "PACKAGE_REPO_URL=" + src_repo.getUrl(),
+                "EXTRA_PACMAN_REPOS=" + repo_manager.getTargetRepo(target_repo).repoToString(),
+                "EXTRA_PACMAN_KEYRINGS=" + repo_manager.getTargetRepo(target_repo).keyringsToBashArray(),
+            ], logger.raw_log.bind(logger));
+
+            const file_list = fs.readdirSync(mount);
+            if (err || out.StatusCode !== undefined || file_list.length === 0) {
+                logger.log(`Job ${job.id} failed`);
+                throw new Error('Building failed.');
+            }
+            else
+                logger.log(`Finished build ${job.id}. Uploading...`);
+
+            const sshlogger = new SshLogger();
+            try {
+                const client = await Client({
+                    host: String(remote_settings.database.ssh.host),
+                    port: Number(remote_settings.database.ssh.port),
+                    username: String(remote_settings.database.ssh.user),
+                    privateKey: fs.readFileSync('sshkey'),
+                    debug: sshlogger.log.bind(sshlogger)
+                })
+                await client.uploadDir(
+                    mount,
+                    remote_settings.database.landing_zone,
+                )
+                client.close()
+            } catch (e) {
+                logger.error(`Failed to upload ${job.id}: ${e}`);
+                // This does not get logged to redis
+                console.error(sshlogger.dump());
+                console.log("End of ssh log.");
+                throw new Error('Upload failed.');
+            }
+            logger.log(`Finished upload ${job.id}.`);
+            const db_job_data: DatabaseJobData = {
+                arch: jobdata.arch,
+                packages: file_list,
+                timestamp: jobdata.timestamp,
+                commit: jobdata.commit,
+                srcrepo: jobdata.srcrepo,
+            };
+            await database_queue.add("database", db_job_data, {
+                jobId: job.id,
+                removeOnComplete: true,
+                removeOnFail: true,
+            });
+            logger.log(`Build job ${job.id} finished. Scheduled database job at ${new Date().toISOString()}...`);
+        } catch (e) {
+            throw e;
+        } finally {
+            setTimeout(promotePendingDependents.bind(null, jobdata, builds_queue, logger), 1000);
+        }
     }, { connection });
     worker.pause();
 
