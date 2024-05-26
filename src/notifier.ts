bdf9c365e713da8bfb829f341ebcee73fa80ef0d
Nico Jensch <root@dr460nf1r3.org>
2024-05-26 10:06:55 +0200

    feat(notifier): enable Telegram notifications depending on whether env is set

--- src/notifier.ts
+++ src/notifier.ts
@@ -7,26 +7,30 @@ import TelegramBot from "node-telegram-bot-api";
 class Notifier {
     private readonly token: string = ''
     private readonly chatId: string = ''
-    bot: TelegramBot
+    bot: TelegramBot | undefined
 
     constructor() {
         if (process.env.TELEGRAM_BOT_TOKEN !== undefined && process.env.TELEGRAM_CHAT_ID !== undefined) {
             this.token = process.env.TELEGRAM_BOT_TOKEN
             this.chatId = process.env.TELEGRAM_CHAT_ID
             this.bot = new TelegramBot(this.token, {polling: false});
-        } else {
-            throw new Error("TELEGRAM_BOT_TOKEN and TELEGRAM_CHAT_ID must be set")
         }
     }
 
     /**
-     * Notifies the given Telegram Chat with the given message.
+     * Notifies the enabled targets with the given message.
      *
-     * @param message The message to send
+     * @param message The message to be sent to each target
+     * @returns Promise<void> Resolves after each target has been notified successfully
      */
-    async notify(message: string) {
+    async notify(message: string): Promise<void> {
         try {
-            await this.bot.sendMessage(this.chatId, message);
+            if (this.bot !== undefined) {
+                // While MarkdownV2 is the non-legacy parse mode, it is annoying to work with because
+                // some characters like "-" in package names need to be escaped. We don't need the extra
+                // features, so lets go with the simpler Markdown parse mode.
+                await this.bot.sendMessage(this.chatId, message, {parse_mode: "Markdown"});
+            }
         } catch (e) {
             console.error("An error occurred:", e)
         }
